eb2764653032ad1fa3d36da991fbfd6b5007bd29
RituP14 <ritupadaki13@gmail.com>
2021-03-19 19:25:47 +0100

    created list component

--- monthlychip/src/App.js
+++ monthlychip/src/App.js
@@ -1,10 +1,28 @@
-import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import itemList from "./components/MonthlyItemsList";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">Learn React</header>
-    </div>
+    <Router>
+      <div className="navBar-list">
+        <h1>Monthly Planner</h1>
+        {/* <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <ul className="navbar-nav mr-auto">
+            <li>
+              <Link to={"/"} className="nav-link">
+                {" "}
+                List{" "}
+              </Link>
+            </li>
+          </ul>
+        </nav> */}
+        <hr />
+        <Switch>
+          <Route exact path="/" component={itemList} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ monthlychip/src/components/MonthlyItemsList.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class MonthlyItemsList extends Component {
+  render() {
+    return (
+      <div className="main">
+        <h1>Heyyy</h1>
+      </div>
+    );
+  }
+}
